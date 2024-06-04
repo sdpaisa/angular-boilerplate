9e6c92aa3ee94fa241773d15ea0f12091dc712a3
sdpaisa <sdpaisa@gmail.com>
2024-06-04 06:04:40 -0500

    feat: :tada: add structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home.component').then((m) => m.HomeComponent),
+        title: 'Inicio',
+      },
+      {
+        path: 'nosotros',
+        loadComponent: () =>
+          import('./pages/aboutus/aboutus.component').then(
+            (m) => m.AboutusComponent,
+          ),
+        title: 'Nosotros',
+      },
+    ],
+  },
+];
---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
